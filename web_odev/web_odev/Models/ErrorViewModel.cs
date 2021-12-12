c19bbb145d3d183ec2fb48d03e8afb1b9278632e
raifakyol <raifakyol61@hotmail.com>
2021-12-12 22:45:10 +0300

    2021-2022

---
+++ web_odev/web_odev/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace web_odev.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
