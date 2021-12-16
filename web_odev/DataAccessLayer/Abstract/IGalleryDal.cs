afa7bf3172933cc092410b7fbfd147aed86e1b5f
raifakyol <raifakyol61@hotmail.com>
2021-12-16 22:58:57 +0300

    2021-2022

---
+++ web_odev/DataAccessLayer/Abstract/IGalleryDal.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGalleryDal
+    {
+        List<Gallery> ListAllGallery();
+        void AddGallery(Gallery gallery);
+        void DeleteGallery(Gallery gallery);
+        void UpdateGallery(Gallery gallery);
+        Gallery GetById(int id);
+    }
+}
