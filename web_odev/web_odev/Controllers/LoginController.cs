571a9bf631cee5d8bfe70583338d657bce0e1774
raifakyol <raifakyol61@hotmail.com>
2021-12-17 23:42:48 +0300

    2021-2022

---
+++ web_odev/web_odev/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace web_odev.Controllers
+{
+    public class LoginController : Controller
+    {
+        Context c = new Context();
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login(Admin p)
+        {
+            var datavalue = c.Admins.FirstOrDefault(x => x.Admin_UserName == p.Admin_UserName && x.Password == p.Password);
+            if (datavalue != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,p.Admin_UserName)
+                };
+                var useridentity = new ClaimsIdentity(claims, "Login");
+                ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
+                await HttpContext.SignInAsync(principal);
+                return RedirectToAction("ServisRandevu", "Home");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> LogOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Login");
+        }
+    }
+}
---
+++ web_odev/web_odev/Controllers/RegisterController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_odev.Controllers
+{
+    public class RegisterController : Controller
+    {
+        AdminManager adm = new AdminManager(new AdminRepository());
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(Admin a)
+        {
+            a.AdminRole = "B";
+            adm.AdminAdd(a);
+
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
