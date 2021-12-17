6d3ed4d44ad95a74c1e031ed4fb21de8382c37df
raifakyol <raifakyol61@hotmail.com>
2021-12-17 23:45:24 +0300

    2021-2022

---
+++ web_odev/BusinessLayer/Concrete/AdminManager.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AdminManager : IAdminService
+    {
+        IAdminDal _admindal;
+        public AdminManager(IAdminDal admindal)
+        {
+            _admindal = admindal;
+        }
+        public void AdminAdd(Admin admin)
+        {
+            _admindal.Insert(admin);
+        }
+    }
+}
