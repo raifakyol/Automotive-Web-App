3406fd770d5b3391566a268a3edefa11451c1410
raifakyol <raifakyol61@hotmail.com>
2021-12-17 23:43:52 +0300

    2021-2022

---
+++ web_odev/DataAccessLayer/Repositories/AdminRepository.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class AdminRepository : IAdminDal
+    {
+        public void Insert(Admin admin)
+        {
+            using var c = new Context();
+            c.Add(admin);
+            c.SaveChanges();
+        }
+    }
+}
