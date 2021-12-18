36a8f47cdf0d2cbd97f1e71f9ba1e7cc313d9458
raifakyol <raifakyol61@hotmail.com>
2021-12-18 23:33:36 +0300

    2021-2022

---
+++ web_odev/web_odev/Areas/Admin/Controllers/FiyatController.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_odev.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class FiyatController : Controller
+    {
+        AracManager arcm = new AracManager(new AracRepository());
+        public IActionResult Index()
+        {
+            var values = arcm.GetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddArac()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddArac(Arac a)
+        {
+            arcm.AracAdd(a);
+            return RedirectToAction("Index", "Fiyat");
+        }
+    }
+}
