e1e35bfb4156dcd1e6c7a4ddb59a9e6ad9084b2b
raifakyol <raifakyol61@hotmail.com>
2021-12-25 22:56:13 +0300

    2021-2022

---
+++ web_odev/BusinessLayer/Abstract/IKullaniciService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IKullaniciService
+    {
+        void KullaniciAdd(Kullanici kullanici);
+        void KullaniciDelete(Kullanici kullanici);
+        void KullaniciUpdate(Kullanici kullanici);
+        List<Kullanici> GetList();
+        Kullanici GetById(int id);
+    }
+}
