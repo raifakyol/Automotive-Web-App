5512ab18c3c4f30d8bb21749f2517b161e3e40a7
raifakyol <raifakyol61@hotmail.com>
2021-12-25 23:00:03 +0300

    2021-2022

---
+++ web_odev/BusinessLayer/Concrete/KullaniciManager.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class KullaniciManager : IKullaniciService
+    {
+        IKullaniciDal _kullanicidal;
+        public KullaniciManager(IKullaniciDal kullanicidal)
+        {
+            _kullanicidal = kullanicidal;
+        }
+        public Kullanici GetById(int id)
+        {
+            return _kullanicidal.GetById(id);
+        }
+
+        public List<Kullanici> GetList()
+        {
+            return _kullanicidal.ListAllKullanici();
+        }
+
+        public void KullaniciAdd(Kullanici kullanici)
+        {
+            _kullanicidal.AddKullanici(kullanici);
+        }
+
+        public void KullaniciDelete(Kullanici kullanici)
+        {
+            _kullanicidal.DeleteKullanici(kullanici);
+        }
+
+        public void KullaniciUpdate(Kullanici kullanici)
+        {
+            _kullanicidal.UpdateKullanici(kullanici);
+        }
+    }
+}
